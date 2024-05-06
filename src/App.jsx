37741971fa37c3462cf4d9c06ea502b150d7459e
Maximo Bautista Garcia <maximogarciadev@gmail.com>
2024-05-06 14:24:57 -0300

    Agregando el footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage"
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path="/" Component={HomePage}/>
         </Routes>
+        <Footer></Footer>
       </div>
     </Router>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import mailIcon from "../assets/mail-icon.png";
+import phoneIcon from "../assets/phone-icon.png";
+import logoMonocromo from "../assets/logo-monocromo-cobre.png";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <h3>Contactanos</h3>
+        <span className="contact-item">
+          <img src={mailIcon} alt="mail icon" />
+          <a href="mailto:contacto@dgfootball.com">contacto@dgfootball.com</a>
+        </span>
+        <span className="contact-item">
+          <img src={phoneIcon} alt="mail icon" />
+          <a>contacto@dgfootball.com</a>
+        </span>
+      </div>
+      <div className="footer-bottom">
+        <p>Todos los derechos reservados © DG Football Agency</p>
+        <img src={logoMonocromo} alt="" />
+        <span>
+          <p>Sitio diseñado y desarrollado por </p>
+          <a href="https://maximogarciadev.com" target="_blank">
+            Maximo B. Garcia
+          </a>
+        </span>
+      </div>
+    </footer>
+  );
+}
