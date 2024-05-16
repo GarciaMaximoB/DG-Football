9ab98f99867f8548fa7a0c9afc67de6aa1583f0e
Maximo Bautista Garcia <maximogarciadev@gmail.com>
2024-05-16 16:37:32 -0300

    solucionando error de la barra del timeline

--- src/components/Players.js
+++ src/components/Players.js
@@ -2,76 +2,75 @@ import capassoImage from "../assets/players/manuel-capasso.webp";
 import pilludImage from "../assets/players/ivan-pillud.webp";
 import campiImage from "../assets/players/gaston-campi.webp";
 import moschionImage from "../assets/players/tomas-moschion.webp";
-import gaggiImage from "../assets/players/enzo-gaggi.webp"
-import nadalinImage from "../assets/players/facundo-nadalin.webp"
-import brizuelaImage from "../assets/players/joaquin-brizuela.webp"
-import licha from "../assets/players/licha.webp"
-import raffinImage from "../assets/players/luca-raffin.webp"
-import borgninoImage from "../assets/players/marco-borgnino.webp"
-import cavalleriImage from "../assets/players/matias-cavalleri.webp"
-import tisseraImage from "../assets/players/matias-tissera.webp"
-import nunezImage from "../assets/players/walter-nuñez.webp"
-
+import gaggiImage from "../assets/players/enzo-gaggi.webp";
+import nadalinImage from "../assets/players/facundo-nadalin.webp";
+import brizuelaImage from "../assets/players/joaquin-brizuela.webp";
+// import licha from "../assets/players/licha.webp";
+import raffinImage from "../assets/players/luca-raffin.webp";
+import borgninoImage from "../assets/players/marco-borgnino.webp";
+import cavalleriImage from "../assets/players/matias-cavalleri.webp";
+import tisseraImage from "../assets/players/matias-tissera.webp";
+import nunezImage from "../assets/players/walter-nuñez.webp";
 
 export const Players = [
   {
     name: "Enzo Gaggi",
     team: "",
-    image: gaggiImage
+    image: gaggiImage,
   },
   {
     name: "Facundo Nadalin",
     team: "",
-    image: nadalinImage
+    image: nadalinImage,
   },
   {
     name: "Gaston Campi",
     team: "",
-    image: campiImage
+    image: campiImage,
   },
   {
     name: "Ivan Pillud",
     team: "",
-    image: pilludImage
+    image: pilludImage,
   },
   {
     name: "Joaquin Brizuela",
     team: "",
-    image: brizuelaImage
+    image: brizuelaImage,
   },
   {
     name: "Luca Raffin",
     team: "",
-    image: raffinImage 
+    image: raffinImage,
   },
   {
     name: "Manuel Capasso",
     team: "",
-    image: capassoImage
+    image: capassoImage,
   },
   {
     name: "Marco Borgnino",
     team: "",
-    image: borgninoImage 
+    image: borgninoImage,
   },
   {
     name: "Matias Cavalleri",
     team: "",
-    image: cavalleriImage
+    image: cavalleriImage,
   },
   {
     name: "Matias Tissera",
     team: "",
-    image: tisseraImage 
+    image: tisseraImage,
   },
   {
     name: "Tomas Moschion",
     team: "",
-    image: moschionImage 
+    image: moschionImage,
   },
   {
     name: "Walter Nuñez",
     team: "",
-    image: nunezImage 
-  }
+    image: nunezImage,
+  },
 ];
