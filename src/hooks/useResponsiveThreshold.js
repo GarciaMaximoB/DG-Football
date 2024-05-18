5f53ed54fa1c247cf862eea30d2080a648e4aeb3
Maximo Bautista Garcia <maximogarciadev@gmail.com>
2024-05-18 19:01:27 -0300

    Modificando responsive de las transferencias

---
+++ src/hooks/useResponsiveThreshold.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+const useResponsiveThreshold = () => {
+  const [threshold, setThreshold] = useState(0.5);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 640) {
+        setThreshold(0.15); // Valor para pantallas pequeñas
+      } else if (window.innerWidth < 1024) {
+        setThreshold(0.5); // Valor para pantallas medianas
+      } else {
+        setThreshold(0.5); // Valor para pantallas grandes
+      }
+    };
+
+    // Escuchar cambios de tamaño de la ventana
+    window.addEventListener("resize", handleResize);
+
+    // Establecer el valor inicial
+    handleResize();
+
+    // Limpiar el event listener al desmontar el componente
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return threshold;
+};
+
+export default useResponsiveThreshold;
--- src/pages/TransferPage.jsx
+++ src/pages/TransferPage.jsx
@@ -1,6 +1,7 @@
 import { Transfers } from "../components/Transfers";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
+import useResponsiveThreshold from "../hooks/useResponsiveThreshold";
 
 export default function TransferPage() {
   return (
@@ -16,19 +17,20 @@ export default function TransferPage() {
 }
 
 const TimelineYear = ({ year }) => {
+  const threshold = useResponsiveThreshold();
   const [ref, inView] = useInView({
     triggerOnce: false,
-    threshold: 0.5,
+    threshold: threshold,
   });
 
   return (
     <motion.div
       className="grid"
       ref={ref}
-      initial={{ opacity: 0, scale: 0.5 }} 
-      animate={{ opacity: inView ? 1 : 0, scale: inView ? 1 : 0.5 }} 
-      exit={{ opacity: 0, scale: 0.5 }} 
-      transition={{ duration: 0.5 }} 
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: inView ? 1 : 0, scale: inView ? 1 : 0.5 }}
+      exit={{ opacity: 0, scale: 0.5 }}
+      transition={{ duration: 0.5 }}
     >
       <div className="grid-left">
         <p className="year-timeline">{year.year}</p>
