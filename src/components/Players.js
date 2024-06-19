5c5efd14d24741153023b561780a6ef99d6d2fd0
Maximo Bautista Garcia <maximogarciadev@gmail.com>
2024-06-19 12:03:00 -0300

    Haciendo cambios solicitados

--- src/components/Players.js
+++ src/components/Players.js
@@ -11,44 +11,35 @@ import borgninoImage from "../assets/players/marco-borgnino.webp";
 import cavalleriImage from "../assets/players/matias-cavalleri.webp";
 import tisseraImage from "../assets/players/matias-tissera.webp";
 import nunezImage from "../assets/players/walter-nuñez.webp";
+import ramirezImage from "../assets/players/lisandro-ramirez.webp";
 
-
-import cobreloa from "../assets/escudos/cobreloa.webp"
-import gimnasiaMendoza from "../assets/escudos/gimnasia-mendoza.webp"
-import imabari from "../assets/escudos/imabari.webp"
-import ludogorets from "../assets/escudos/ludogorets.webp"
-import montevideoCity from "../assets/escudos/montevideocity.webp"
-import olimpia from "../assets/escudos/olimpia.webp"
-import sanlorenzo from "../assets/escudos/sanlorenzo.webp"
+import cobreloa from "../assets/escudos/cobreloa.webp";
+import gimnasiaMendoza from "../assets/escudos/gimnasia-mendoza.webp";
+import imabari from "../assets/escudos/imabari.webp";
+import ludogorets from "../assets/escudos/ludogorets.webp";
+import montevideoCity from "../assets/escudos/montevideocity.webp";
+import olimpia from "../assets/escudos/olimpia.webp";
+import sanlorenzo from "../assets/escudos/sanlorenzo.webp";
 // import temperley from "../assets/escudos/temperley.webp"
-import unionLaCalera from "../assets/escudos/union-la-calera.webp"
-import delfin from "../assets/escudos/delfin.webp"
-import racing from "../assets/escudos/racing.webp"
-import godoycruz from "../assets/escudos/godoycruz.webp"
-import rosariocentral from "../assets/escudos/rosariocentral.webp"
+import unionLaCalera from "../assets/escudos/union-la-calera.webp";
+import delfin from "../assets/escudos/delfin.webp";
+import racing from "../assets/escudos/racing.webp";
+import godoycruz from "../assets/escudos/godoycruz.webp";
+import rosariocentral from "../assets/escudos/rosariocentral.webp";
+import union from "../assets/escudos/union.webp";
 
 export const Players = [
   {
-    name: "Enzo Gaggi",
-    team: delfin,
-    image: gaggiImage,
-    redes: {
-      transferMarkt: "https://www.transfermarkt.es/enzo-gaggi/profil/spieler/538337",
-      soccerWay: "https://es.soccerway.com/players/enzo-gaggi/485351/",
-      Youtube: " https://youtu.be/Px0JK16957c?si=nNVuRN2P-JquI02C",
-      wyscout: "#",
-    },
-  },
-  {
-    name: "Facundo Nadalin",
-    team: gimnasiaMendoza,
-    image: nadalinImage,
+    name: "Manuel Capasso",
+    team: olimpia,
+    image: capassoImage,
     redes: {
       transferMarkt:
-        "https://www.transfermarkt.com/facundo-nadalin/profil/spieler/554798",
-      soccerWay: "https://es.soccerway.com/players/facundo-nadalin/526523/",
-      Youtube: "https://www.youtube.com/@dgfootball.agency",
-      wyscout: "#",
+        "https://www.transfermarkt.com/manuel-capasso/profil/spieler/653152",
+      soccerWay:
+        "https://es.soccerway.com/players/manuel-vicente-capasso/401403/",
+      Youtube: " https://www.youtube.com/watch?v=IBn_nT0It90&t=17s",
+      wyscout: "https://www.wyscout.com/enzo-gaggi",
     },
   },
   {
@@ -76,54 +67,6 @@ export const Players = [
     },
   },
   {
-    name: "Joaquin Brizuela",
-    team: godoycruz,
-    image: brizuelaImage,
-    redes: {
-      transferMarkt:
-        "#",
-      soccerWay: "#",
-      Youtube: ": https://youtu.be/3-9nsZXv9iA?si=XXvsZ6aMkQwVjLIy",
-      wyscout: "#",
-    },
-  },
-  {
-    name: "Luca Raffin",
-    team: rosariocentral,
-    image: raffinImage,
-    redes: {
-      transferMarkt:
-        "https://www.transfermarkt.com/luca-raffin/profil/spieler/1186213",
-      soccerWay: "https://es.soccerway.com/players/luca-valentino-raffin/931281/",
-      Youtube: "https://youtu.be/DqxmkEvPvWI?si=10CC1N5lugt5Gpda",
-      wyscout: "https://www.wyscout.com/enzo-gaggi",
-    },
-  },
-  {
-    name: "Manuel Capasso",
-    team: olimpia,
-    image: capassoImage,
-    redes: {
-      transferMarkt:
-        "https://www.transfermarkt.com/manuel-capasso/profil/spieler/653152",
-      soccerWay: "https://es.soccerway.com/players/manuel-vicente-capasso/401403/",
-      Youtube: " https://www.youtube.com/watch?v=IBn_nT0It90&t=17s",
-      wyscout: "https://www.wyscout.com/enzo-gaggi",
-    },
-  },
-  {
-    name: "Marco Borgnino",
-    team: cobreloa,
-    image: borgninoImage,
-    redes: {
-      transferMarkt:
-        "https://www.transfermarkt.com/marco-borgnino/profil/spieler/458171",
-      soccerWay: "https://es.soccerway.com/players/marco-bognino/446871/",
-      Youtube: "https://youtu.be/VgVXC0xM3CE",
-      wyscout: "https://www.wyscout.com/enzo-gaggi",
-    },
-  },
-  {
     name: "Matias Cavalleri",
     team: unionLaCalera,
     image: cavalleriImage,
@@ -136,17 +79,68 @@ export const Players = [
     },
   },
   {
-    name: "Matias Tissera",
-    team: ludogorets,
-    image: tisseraImage,
+    name: "Enzo Gaggi",
+    team: delfin,
+    image: gaggiImage,
     redes: {
       transferMarkt:
-        " https://www.transfermarkt.com/matias-tissera/profil/spieler/503001",
-      soccerWay: "https://es.soccerway.com/players/matias-tissera/478312/",
-      Youtube: " https://youtu.be/ff68nEcBlUA",
+        "https://www.transfermarkt.es/enzo-gaggi/profil/spieler/538337",
+      soccerWay: "https://es.soccerway.com/players/enzo-gaggi/485351/",
+      Youtube: " https://youtu.be/Px0JK16957c?si=nNVuRN2P-JquI02C",
+      wyscout: "#",
+    },
+  },
+  {
+    name: "Facundo Nadalin",
+    team: gimnasiaMendoza,
+    image: nadalinImage,
+    redes: {
+      transferMarkt:
+        "https://www.transfermarkt.com/facundo-nadalin/profil/spieler/554798",
+      soccerWay: "https://es.soccerway.com/players/facundo-nadalin/526523/",
+      Youtube: "https://www.youtube.com/@dgfootball.agency",
+      wyscout: "#",
+    },
+  },
+
+  {
+    name: "Joaquin Brizuela",
+    team: godoycruz,
+    image: brizuelaImage,
+    redes: {
+      transferMarkt: "#",
+      soccerWay: "#",
+      Youtube: ": https://youtu.be/3-9nsZXv9iA?si=XXvsZ6aMkQwVjLIy",
+      wyscout: "#",
+    },
+  },
+  {
+    name: "Luca Raffin",
+    team: rosariocentral,
+    image: raffinImage,
+    redes: {
+      transferMarkt:
+        "https://www.transfermarkt.com/luca-raffin/profil/spieler/1186213",
+      soccerWay:
+        "https://es.soccerway.com/players/luca-valentino-raffin/931281/",
+      Youtube: "https://youtu.be/DqxmkEvPvWI?si=10CC1N5lugt5Gpda",
       wyscout: "https://www.wyscout.com/enzo-gaggi",
     },
   },
+
+  {
+    name: "Marco Borgnino",
+    team: cobreloa,
+    image: borgninoImage,
+    redes: {
+      transferMarkt:
+        "https://www.transfermarkt.com/marco-borgnino/profil/spieler/458171",
+      soccerWay: "https://es.soccerway.com/players/marco-bognino/446871/",
+      Youtube: "https://youtu.be/VgVXC0xM3CE",
+      wyscout: "https://www.wyscout.com/enzo-gaggi",
+    },
+  },
+
   {
     name: "Tomas Moschion",
     team: imabari,
@@ -155,8 +149,20 @@ export const Players = [
       transferMarkt:
         "https://www.transfermarkt.com/tomas-moschion/profil/spieler/631887",
       soccerWay: "https://es.soccerway.com/players/tomas-moschion/580026/",
-      Youtube: "https://www.youtube.com/watch?v=8PqIMEz5_EY&pp=ygUOdG9tYXMgbW9zY2hpb24%3D",
+      Youtube:
+        "https://www.youtube.com/watch?v=8PqIMEz5_EY&pp=ygUOdG9tYXMgbW9zY2hpb24%3D",
       wyscout: "https://www.wyscout.com/enzo-gaggi",
+    },
+  },
+  {
+    name: "Lisandro Ramirez",
+    team: union,
+    image: ramirezImage,
+    redes: {
+      transferMarkt: "#",
+      soccerWay: "#",
+      Youtube: "#",
+      wyscout: "#",
     },
   },
   {
@@ -168,6 +174,19 @@ export const Players = [
         " https://www.transfermarkt.com.ar/walter-nunez/profil/spieler/1020838",
       soccerWay: "https://www.soccerway.com/",
       Youtube: "https://youtu.be/rluWxqL5yBk",
+      wyscout: "https://www.wyscout.com/enzo-gaggi",
+    },
+  },
+
+  {
+    name: "Matias Tissera",
+    team: ludogorets,
+    image: tisseraImage,
+    redes: {
+      transferMarkt:
+        " https://www.transfermarkt.com/matias-tissera/profil/spieler/503001",
+      soccerWay: "https://es.soccerway.com/players/matias-tissera/478312/",
+      Youtube: " https://youtu.be/ff68nEcBlUA",
       wyscout: "https://www.wyscout.com/enzo-gaggi",
     },
   },
